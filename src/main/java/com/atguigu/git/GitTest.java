5d1cbdbafd1f577dfee7bdbafbbf6ef23702d6eb
Amelia <2443684780@qq.com>
2021-09-14 17:00:38 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @program: git-test
+ * @ClassName GitTest
+ * @description:
+ * @author: 付帅
+ * @create: 2021-09-14 16:50
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
